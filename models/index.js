541461fccb97a89d1698f200de67e97c0fac6040
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-03-17 17:55:01 -0500

    fixed seed file

--- models/index.js
+++ models/index.js
@@ -4,29 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Categories have many Products
-Category.hasMany(Product,{
-  foreignKey: 'category_id',
-  OnDelete: 'CASCADE'
-})
-
 // Products belongsTo Category
-Product.belongsTo(Category,{
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id'
 });
 
-
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product,{
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
-})
+});
 
 module.exports = {
   Product,
